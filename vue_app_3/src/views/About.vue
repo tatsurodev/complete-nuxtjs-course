8eebd80a842915281a268b66bedd88490db997eb
Tatsuro <contacts@tatsuro.dev>
2019-06-14 14:04:20 +0900

    vue_app_3プロジェクト作成

---
+++ vue_app_3/src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld
+  }
+};
+</script>

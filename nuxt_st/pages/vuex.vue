4587ca80f05830081b6ee2e31b1f3c28bd4fea0a
Tatsuro <contacts@tatsuro.dev>
2019-06-15 02:54:06 +0900

    classic modeでvuexを作成

---
+++ nuxt_st/pages/vuex.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="vuex">{{ $store.state.msg }}</div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+#vuex {
+  font-size: 22px;
+  padding: 10px;
+}
+</style>
---
+++ nuxt_st/store/index.js
@@ -0,0 +1,22 @@
+import vuex from 'vuex'
+// vue的手法で必要
+// import vue from 'vue'
+// vue.use(vuex)
+
+const store = {
+  state: {
+    msg: 'msg in store/index.js state'
+  },
+  getters: {},
+  mutations: {},
+  actions: {}
+}
+
+// vue的手法ではこの方法でexport
+// vuex.storeメソッドでインスタンス作成してexport
+// export default new vuex.Store(store)
+
+// nuxtでは無名関数でexport
+export default function () {
+  return new vuex.Store(store)
+}

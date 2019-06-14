7ff2b9f694122c083938f2db79eb3a7db90a87a6
Tatsuro <contacts@tatsuro.dev>
2019-06-14 21:36:03 +0900

    custom layoutを作成して、index.vueで使用する 12

---
+++ nuxt_st/layouts/my_layout.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div id="header">
+      <b>
+        <i>My Layout Header</i>
+      </b>
+    </div>
+    <!-- custom layoutの作成ではroot componentを表示するためのnuxtタグとcssのscopedが必須 -->
+    <nuxt/>
+    <div id="footer">
+      <b>
+        <i>My Layout Footer</i>
+      </b>
+    </div>
+  </div>
+</template>
+<style scoped>
+#header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 50px;
+  width: 100%;
+  background: orange;
+  line-height: 50px;
+  z-index: 999;
+  text-align: center;
+  color: white;
+  font-size: 20px;
+}
+#main {
+  padding-top: 50px;
+  padding-bottom: 45px;
+}
+#footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 45px;
+  width: 100%;
+  background: teal;
+  font-size: 18px;
+  line-height: 45px;
+  text-align: center;
+  color: white;
+}
+</style>
--- nuxt_st/pages/index.vue
+++ nuxt_st/pages/index.vue
@@ -2,34 +2,26 @@
   <section class="container">
     <div>
       <app-logo/>
-      <h1 class="title">
-        nuxt_st
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
+      <h1 class="title">nuxt_st</h1>
+      <h2 class="subtitle">Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
+  // layoutの指定がない場合、default.vueが読み込まれる
+  layout: "my_layout",
   components: {
     AppLogo
   }
-}
+};
 </script>
 
 <style>
@@ -42,7 +34,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
@@ -62,4 +55,3 @@ export default {
   padding-top: 15px;
 }
 </style>
-

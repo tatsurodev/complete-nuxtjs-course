97a3c593a19580425519b3de9419fa558baaeea4
Tatsuro <contacts@tatsuro.dev>
2019-06-14 21:24:21 +0900

    カスタムエラーページを作成 11

---
+++ nuxt_st/layouts/error.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="error">
+    <!-- <app-logo/> -->
+    <AppLogo/>
+    <div id="errorStatus">{{ error.statusCode }}</div>
+    <div id="errorMsg">{{ error.message }}</div>
+  </div>
+</template>
+<script>
+import AppLogo from "@/components/AppLogo";
+export default {
+  // 使用するlayoutをdefaultからカスタムエラー用のものに変更
+  layout: "error_layout",
+  components: {
+    AppLogo
+  },
+  props: ["error"]
+};
+</script>
+<style>
+#error {
+  margin: 20px;
+  text-align: center;
+  margin-top: 50px;
+}
+#errorStatus {
+  font-family: monospace;
+  font-size: 42px;
+  background: red;
+  color: white;
+  line-height: 60px;
+  margin: 10px 15px;
+}
+#errorMsg {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-seif;
+  font-size: 24px;
+  line-height: 36px;
+}
+</style>
---
+++ nuxt_st/layouts/error_layout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div id="header">Header: error_layout.vue in layouts</div>
+
+    <div id="main">
+      <nuxt/>
+    </div>
+
+    <div id="footer">Footer error_layout.vue in layouts</div>
+  </div>
+</template>
+<style scoped>
+/* scopedがないと、layoutフォルダ内のファイルが使用、不使用に関わらずアルファベット順に読み込まれるので、同様のcss要素が指定してあると他のlayoutファイルのcssが効いてしまう可能性があるので、scopedをつけてcssの汚染を防ぐ */
+#header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 50px;
+  width: 100%;
+  background: red;
+  line-height: 50px;
+  z-index: 999;
+  text-align: center;
+  color: white;
+  font-size: 20px;
+}
+#main {
+  padding-top: 50px;
+  padding-bottom: 45px;
+}
+#footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 45px;
+  width: 100%;
+  background: red;
+  font-size: 18px;
+  line-height: 45px;
+  text-align: center;
+  color: white;
+}
+</style>

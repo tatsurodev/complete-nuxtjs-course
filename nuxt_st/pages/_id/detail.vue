974e71b7e3eaae4ea7c22d4a35ad7b0d706d9be9
Tatsuro <contacts@tatsuro.dev>
2019-06-16 07:09:01 +0900

    dynamic routeのパラメータ部分をurl末尾から途中に変更する処理を実装 35

---
+++ nuxt_st/pages/_id/detail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="padding: 15px">
+    <b-card border-variant="success" footer-bg-variant="success" text-variant="success">
+      <p>
+        <i class="fas fa-file-code"></i>
+        <span>pages/_id/detail.vue</span>
+      </p>
+      <b-alert show variant="success">
+        <h3>Detail Page</h3>
+        <i class="fas fa-id-card-alt"></i>
+        <b>ID:</b>
+        <b>{{ $route.params.id }}</b>
+        <hr>
+        <nuxt-link :to="{ name: 'id', params: { id: $route.params.id } }">ID Page</nuxt-link>
+      </b-alert>
+      <div slot="footer">
+        <span style="color:white">Dynamic Route</span>
+      </div>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  transition: {
+    name: "prefix"
+  },
+  head() {
+    return {
+      title: "Dynamic Route"
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ nuxt_st/pages/_id/index.vue
@@ -0,0 +1,36 @@
+<!-- urlがlocalhost:3000/id/の時、/pages/id/index.vueと/pages/_id/index.vueが参照されるが、エラーページ用のid/index.vueよりdynamic routeの_id/index.vueの方が優先される -->
+<template>
+  <div style="padding: 15px">
+    <b-card border-variant="info" footer-bg-variant="info" text-variant="info">
+      <p>
+        <i class="fas fa-file-code"></i>
+        <span>pages/_id/index.vue</span>
+      </p>
+      <b-alert show variant="info">
+        <i class="fas fa-id-card-alt"></i>
+        <b>ID:</b>
+        <b>{{ $route.params.id }}</b>
+        <hr>
+        <!-- 名前付きrouteは、folder-file_nameで、paramsでidの動的パラメータ部を渡す -->
+        <nuxt-link :to="{ name: 'id-detail', params: { id: $route.params.id } }">Detail Page</nuxt-link>
+      </b-alert>
+      <div slot="footer">
+        <span style="color:white">Dynamic Route</span>
+      </div>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  transition: {
+    name: "prefix"
+  },
+  head() {
+    return {
+      title: "Dynamic Route"
+    };
+  }
+};
+</script>
+<style scoped>
+</style>

d5e3414966c9017304b2e895547be49e852f5538
Tatsuro <contacts@tatsuro.dev>
2019-06-16 06:24:30 +0900

    id vueをdynamic route で作成 34

---
+++ nuxt_st/pages/id/_id.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="padding: 15px">
+    <b-card border-variant="primary" footer-bg-variant="primary" text-variant="primary">
+      <p>
+        <i class="fas fa-file-code"></i>
+        <span>pages/id/_id.vue</span>
+      </p>
+      <b-alert show variant="primary">
+        <i class="fas fa-id-card-alt"></i>
+        <b>ID:</b>
+        <!-- dynamic routeでparamsがない場合nullとして扱われ普通に’ページ表示できてしまうので、/pages/id/index.vueを作成し、paramsなしの場合に表示するエラーページを作成する -->
+        <b>{{ $route.params.id }}</b>
+      </b-alert>
+      <div slot="footer">
+        <span style="color:white">Dynamic Route</span>
+      </div>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  transition: {
+    name: "prefix"
+  },
+  head() {
+    return {
+      title: "Dynamic Route"
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ nuxt_st/pages/id/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="padding: 15px">
+    <b-card border-variant="danger" footer-bg-variant="danger" text-variant="danger">
+      <p>
+        <i class="fas fa-file-code"></i>
+        <span>pages/id/index.vue</span>
+      </p>
+      <b-alert show variant="danger">
+        <i class="fas fa-id-card-alt"></i>
+        <b>ID:</b>
+        <b>No ID Provided!</b>
+      </b-alert>
+      <div slot="footer">
+        <span style="color:white">Dynamic Route</span>
+      </div>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  transition: {
+    name: "prefix"
+  },
+  head() {
+    return {
+      title: "Dynamic Route"
+    };
+  }
+};
+</script>
+<style scoped>
+</style>

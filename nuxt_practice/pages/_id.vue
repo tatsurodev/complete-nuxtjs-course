16de2b9ead2302b603b2709b44f73094c48326b3
Tatsuro <contacts@tatsuro.dev>
2019-06-20 21:11:57 +0900

    gallery componentで画像データをfetch 61

--- nuxt_practice/pages/_id.vue
+++ nuxt_practice/pages/_id.vue
@@ -18,6 +18,26 @@ export default {
     } else {
       throw new Error("Illegal ID Value...");
     }
+  },
+  fetch(context) {
+    context.$axios
+      .get("http://localhost:9000/image/" + context.params.id)
+      .then(res => {
+        // 画像取得成功
+        if (res.data[0]) {
+          // context.params.idが車のid, res.data[1]が車の画像パス
+          context.store.commit("set_image", [context.params.id, res.data[1]]);
+          // 画像取得失敗で失敗画像のプレースホルダー
+        } else {
+          context.store.commit("set_image", [
+            context.params.id,
+            "./error.jpeg"
+          ]);
+        }
+      })
+      .catch(err => {
+        context.store.commit("set_image", [context.params.id, "./error.jpeg"]);
+      });
   }
 };
 </script>
--- nuxt_practice/store/index.js
+++ nuxt_practice/store/index.js
@@ -1,3 +1,8 @@
+// export const state = () => {
+//   return {sample: 'this is object literal'}
+// }
+// 上記のと下記のstateは同意
+// 上記は、アロー関数で一行の処理をreturnしているので { return } が省略できる。よって中身のオブジェクトリテラルの {sample: 'this is object literal } のみを記述することになるが、スコープの {} と混同するので {} の外に () を追加する。アロー関数でオブジェクトリテラルを返す時、({}) とすることに注意
 export const state = () => ({
   list: []
 });
@@ -5,6 +10,12 @@ export const state = () => ({
 export const mutations = {
   set_list(state, arg) {
     state.list = arg
+  },
+  set_image(state, arg) {
+    // 車データのオブジェクト入ったstore.listの配列に画像データを追加する
+    // arg[0] = context.params.id
+    // arg[1] = imagePromise.data[1]
+    state.list[arg[0]].image = arg[1]
   }
 }
 
